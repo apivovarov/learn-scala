73d633e7a8192d7ac1e86d0ff854c3d1ca38aecb
Alexander Pivovarov <apivovarov@gmail.com>
2015-07-05 00:27:22 -0700

    add gcd to Rational

--- src/learnscala/lang/Rational.scala
+++ src/learnscala/lang/Rational.scala
@@ -3,9 +3,15 @@ package learnscala.lang
 class Rational(n: Int, d: Int) {
   require(d != 0)
 
-  val nu = n
-  val dn = d
-  override def toString = n + "/" + d
+  private val g = gcd(n.abs, d.abs)
+  println(g)
+
+  val nu = n / g
+  val dn = d / g
+
+  def this(n: Int) = this(n, 1) // auxiliary constructor
+
+  override def toString = nu + "/" + dn
 
   def add(that: Rational): Rational = new Rational(
     nu * that.dn + that.nu * dn, dn * that.dn)
@@ -13,4 +19,9 @@ class Rational(n: Int, d: Int) {
   def lessThan(that: Rational) =
     this.nu * that.dn < that.nu * this.dn
 
+  def max(that: Rational) =
+    if (this.lessThan(that)) that else this
+
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
 }
