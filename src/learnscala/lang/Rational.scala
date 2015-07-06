d6c5139389aa3af4dc2a6eac5add649c2ef4effc
Alexander Pivovarov <apivovarov@gmail.com>
2015-07-05 22:47:08 -0700

    add + operation to Rational

--- src/learnscala/lang/Rational.scala
+++ src/learnscala/lang/Rational.scala
@@ -4,7 +4,6 @@ class Rational(n: Int, d: Int) {
   require(d != 0)
 
   private val g = gcd(n.abs, d.abs)
-  println(g)
 
   val nu = n / g
   val dn = d / g
@@ -13,15 +12,29 @@ class Rational(n: Int, d: Int) {
 
   override def toString = nu + "/" + dn
 
-  def add(that: Rational): Rational = new Rational(
+  def +(that: Rational): Rational = new Rational(
     nu * that.dn + that.nu * dn, dn * that.dn)
 
-  def lessThan(that: Rational) =
+  def +(i: Int): Rational = new Rational(
+    dn * i + nu, dn)
+
+  def <(that: Rational) =
     this.nu * that.dn < that.nu * this.dn
 
   def max(that: Rational) =
-    if (this.lessThan(that)) that else this
+    if (this < that) that else this
 
   private def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
+
+  override def equals(o: Any) = o match {
+    case that: Rational => that.nu == nu && that.dn == dn
+    case _              => false
+  }
+}
+
+object Rational extends App {
+  val a = new Rational(2, 5) + 2
+  println(a)
+  assert(a == new Rational(12, 5))
 }
